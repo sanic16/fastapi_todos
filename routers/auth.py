c64b45c8c6c3db5f946eb633cad02ca2c5a62991
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-07-25 17:57:46 -0600

    password match

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -15,8 +20,28 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str 
 
-@router.post('/auth/')
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticated_user(username: str, password: str, db: Session):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    
+    return True
+
+
+@router.post('/auth', status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -26,4 +51,13 @@ async def create_user(create_user_request: CreateUserRequest):
         role= create_user_request.role,
         is_active=True
     )
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
+@router.post('/token')
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticated_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    
+    return  'Successful Authentication'
