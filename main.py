b0262832f0bf00a70d51c9790a8d94622940caf4
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-03 14:21:42 -0600

    cors

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
 from routers import auth, todos, admin, users
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 models.Base.metadata.create_all(bind=engine)
 
